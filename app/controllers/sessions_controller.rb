bdd917d1304a033437acddc6b2602435d289b2a8
Luke Chen <luke.chen.2010@gmail.com>
2020-06-29 02:29:08 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   def create
-    return redirect_to(controller: 'sessions',
-                       action: 'new') if !params[:name] || params[:name].empty?
+    if !params[:name] || params[:name].empty?
   end
 end
