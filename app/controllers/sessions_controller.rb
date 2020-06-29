f7efedb4e526f6e61579072613d30ecbc6156263
Luke Chen <luke.chen.2010@gmail.com>
2020-06-29 02:32:38 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   
   def create
-    if 
-      
+    if session[:name] 
+      redirect_to '/'
     elsif !params[:name] || params[:name] == ""
       redirect_to(controller: 'sessions', action: 'new')
     else
